63a0d2e063c5310010489b06ff3741cf561102f0
lahiruthpala <lahiruthpala@gmail.com>
2025-05-05 22:38:41 +0530

    update

--- src/app/orders/[orderId]/page.tsx
+++ src/app/orders/[orderId]/page.tsx
@@ -7,7 +7,7 @@ type OrderDetails = Awaited<ReturnType<typeof getOrderById>>;
 export default async function OrderPage({params,}: {params: Params; }) {
   const { orderId } = await params
   const orderDetails:OrderDetails = await getOrderById(orderId)
-
+    console.log(orderDetails)
   return (
     <div className="container mx-auto py-10">
       <div className="max-w-2xl mx-auto">
